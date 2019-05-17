e731673a9d17f1d957114abf7386f5bb55119710
Evgeniy Filippovich <filippovich.z@mail.ru>
2019-05-17 15:52:43 +0300

    add Main and Input

--- task3/src/Main.java
+++ task3/src/Main.java
@@ -1,21 +1,35 @@
 import entity.Matrix;
 import logic.*;
 
-import java.util.HashSet;
-import java.util.Set;
+import static util.UserInput.*;
 
 public class Main {
     public static void main(String[] args) {
-        Matrix matrix = new Matrix(3, 3);
-        Creator.initialize(matrix);
-        System.out.println("Generated matrix:");
-        System.out.println(matrix);
-        int[] mas = LocalExtremes.findLocalMaximum(matrix);
-        execute(mas);
-        TransposeMatrix.transpose(matrix);
-        System.out.println(matrix);
+        System.out.println("Input count rows(N) and count columns(M):");
+        int n = readInt();
+        int m = readInt();
+        if (n > 0 && m > 0) {
+            Matrix matrix = new Matrix(n, m);
+            Creator.initialize(matrix);
+            System.out.println("Generated matrix(N*M):");
+            System.out.println(matrix);
+            System.out.println("Arithmetic mean = " + ArithmeticMeanAndGeometric.calculateArithmeticMean(matrix));
+            System.out.println("Geometric mean = " + ArithmeticMeanAndGeometric.calculateGeometricMean(matrix));
+            System.out.println("Minimum = " + Extremes.findMinimum(matrix));
+            System.out.println("Maximum = " + Extremes.findMaximum(matrix));
+            System.out.println("Local minimum indexes:");
+            int[] mas = LocalExtremes.findLocalMinimum(matrix);
+            execute(mas);
+            mas = LocalExtremes.findLocalMaximum(matrix);
+            System.out.println("Local maximum indexes:");
+            execute(mas);
+            System.out.println("Transpose matrix:");
+            TransposeMatrix.transpose(matrix);
+            System.out.println(matrix);
+        }
     }
-    private static void execute(int[] array){
+
+    private static void execute(int[] array) {
         System.out.println(array[0] + " " + array[1]);
     }
 }
