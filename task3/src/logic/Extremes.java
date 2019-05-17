f74dc5e0810fc5f17e7a7943eb04c27cbdfd9c2c
Evgeniy Filippovich <filippovich.z@mail.ru>
2019-05-17 10:30:45 +0300

    add diagrams

--- task3/src/logic/Extremes.java
+++ task3/src/logic/Extremes.java
@@ -4,7 +4,7 @@ import entity.Matrix;
 
 public class Extremes {
     public static double findMinimum(Matrix matrix) {
-        double minimum = 0;
+        double minimum = -1;
         if (matrix != null) {
             minimum = matrix.getElement(0, 0);
             for (int i = 0; i < matrix.getCountRows(); i++) {
@@ -19,7 +19,7 @@ public class Extremes {
     }
 
     public static double findMaximum(Matrix matrix) {
-        double maximum = 0;
+        double maximum = -1;
         if (matrix != null) {
             for (int i = 0; i < matrix.getCountRows(); i++) {
                 for (int j = 0; j < matrix.getCountColumns(); j++) {
--- task3/src/logic/LocalExtremesMethodTwo.java
+++ task3/src/logic/LocalExtremesMethodTwo.java
@@ -4,7 +4,6 @@ import entity.Matrix;
 
 public class LocalExtremesMethodTwo {
     public static int[] FindMin(Matrix myArr) {
-        double min = myArr.getElement(0, 0);
         int[] indexes = {-1, -1};
         for (int i = 0; i < myArr.getCountRows(); i++) {
             for (int j = 0; j < myArr.getCountColumns(); j++) {
@@ -18,16 +17,16 @@ public class LocalExtremesMethodTwo {
         return indexes;
     }
 
-    private static boolean FindLocalMin(Matrix array, int i, int j) {
-        double min = array.getElement(i, j);
+    private static boolean FindLocalMin(Matrix matrix, int i, int j) {
+        double min = matrix.getElement(i, j);
         int iStart = ((i - 1) >= 0) ? (i - 1) : 0;
-        int iEnd = ((i + 1) < array.getCountRows() - 1) ? (i + 1) : array.getCountRows() - 1;
+        int iEnd = ((i + 1) < matrix.getCountRows() - 1) ? (i + 1) : matrix.getCountRows() - 1;
         int jStart = ((j - 1) >= 0) ? (j - 1) : 0;
-        int jEnd = ((j + 1) < array.getCountColumns() - 1) ? (j + 1) : array.getCountColumns() - 1;
+        int jEnd = ((j + 1) < matrix.getCountColumns() - 1) ? (j + 1) : matrix.getCountColumns() - 1;
 
         for (i = iStart; i <= iEnd; i++) {
             for (j = jStart; j <= jEnd; j++) {
-                if (array.getElement(i, j) < min) {
+                if (matrix.getElement(i, j) < min) {
                     return false;
                 }
             }
@@ -36,7 +35,6 @@ public class LocalExtremesMethodTwo {
     }
 
     public static int[] FindMax(Matrix myArr) {
-        double min = myArr.getElement(0, 0);
         int[] indexes = {-1, -1};
         for (int i = 0; i < myArr.getCountRows(); i++) {
             for (int j = 0; j < myArr.getCountColumns(); j++) {
@@ -50,16 +48,16 @@ public class LocalExtremesMethodTwo {
         return indexes;
     }
 
-    private static boolean FindLocalMax(Matrix array, int i, int j) {
-        double max = array.getElement(i, j);
+    private static boolean FindLocalMax(Matrix matrix, int i, int j) {
+        double max = matrix.getElement(i, j);
         int iStart = ((i - 1) >= 0) ? (i - 1) : 0;
-        int iEnd = ((i + 1) < array.getCountRows() - 1) ? (i + 1) : array.getCountRows() - 1;
+        int iEnd = ((i + 1) < matrix.getCountRows() - 1) ? (i + 1) : matrix.getCountRows() - 1;
         int jStart = ((j - 1) >= 0) ? (j - 1) : 0;
-        int jEnd = ((j + 1) < array.getCountColumns() - 1) ? (j + 1) : array.getCountColumns() - 1;
+        int jEnd = ((j + 1) < matrix.getCountColumns() - 1) ? (j + 1) : matrix.getCountColumns() - 1;
 
         for (i = iStart; i <= iEnd; i++) {
             for (j = jStart; j <= jEnd; j++) {
-                if (array.getElement(i, j) > max) {
+                if (matrix.getElement(i, j) > max) {
                     return false;
                 }
             }
